49e5d23300e9d5d12597b8d701d80a7a216d6d00
Rui Lopes <rgl@ruilopes.com>
2021-04-26 08:54:57 +0100

    import the hello-puppeteer-windows-container repository as a docker(-compose) on windows example

--- create-example-repositories.sh
+++ create-example-repositories.sh
@@ -14,6 +14,7 @@ gitlab-create-project-and-import https://github.com/rgl/ubuntu-vagrant.git ubunt
 gitlab-create-project-and-import https://github.com/rgl/example-dotnet-source-link.git example-dotnet-source-link $example_group_id
 gitlab-create-project-and-import https://github.com/rgl/MailBounceDetector.git MailBounceDetector $example_group_id
 gitlab-create-project-and-import https://github.com/rgl/HelloSeleniumWebDriver.git HelloSeleniumWebDriver $example_group_id
+gitlab-create-project-and-import https://github.com/rgl/hello-puppeteer-windows-container.git hello-puppeteer-windows-container $example_group_id
 
 # configure the git client.
 git config --global user.name "Root Doe"
