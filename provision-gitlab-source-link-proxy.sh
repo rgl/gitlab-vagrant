36940eb255316958d0df4adb20a33ce176206003
Rui Lopes <rgl@ruilopes.com>
2022-03-19 20:07:01 +0000

    upgrade to gitlab-source-link-proxy 0.0.2

--- provision-gitlab-source-link-proxy.sh
+++ provision-gitlab-source-link-proxy.sh
@@ -4,8 +4,8 @@ set -eux
 domain=$(hostname --fqdn)
 
 # download.
-artifact_url=https://github.com/rgl/gitlab-source-link-proxy/releases/download/v0.0.1/gitlab-source-link-proxy.tgz
-artifact_sha=98a1e74e553c6f405e8c420fa714376b65501aa7532b16c64871a8812e40c179
+artifact_url=https://github.com/rgl/gitlab-source-link-proxy/releases/download/v0.0.2/gitlab-source-link-proxy_0.0.2_linux_amd64.tar.gz
+artifact_sha=e0d8001feb5ca27cd56486bc103139a192e3e22f21e2ec5d34108637b9f9a133
 wget -qO /tmp/gitlab-source-link-proxy.tgz $artifact_url
 if [ "$(sha256sum /tmp/gitlab-source-link-proxy.tgz | awk '{print $1}')" != "$artifact_sha" ]; then
     echo "downloaded $artifact_url failed the checksum verification"
@@ -26,7 +26,11 @@ install -d -o root -g gitlab-source-link-proxy -m 750 /opt/gitlab-source-link-pr
 install -d -o root -g gitlab-source-link-proxy -m 750 /opt/gitlab-source-link-proxy/bin
 
 # create the service and start it.
-tar xf /tmp/gitlab-source-link-proxy.tgz -C /opt/gitlab-source-link-proxy/bin
+tar xf /tmp/gitlab-source-link-proxy.tgz \
+    --owner root \
+    --group root \
+    --no-same-owner \
+    -C /opt/gitlab-source-link-proxy/bin
 cat >/etc/systemd/system/gitlab-source-link-proxy.service <<EOF
 [Unit]
 Description=gitlab-source-link-proxy
