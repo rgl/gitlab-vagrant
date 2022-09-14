d87603fa6842cfe7e3a240d74a66a2b09f6a2942
Rui Lopes <rgl@ruilopes.com>
2022-09-14 19:00:19 +0100

    add the gitlab-runner-environment-info-k8s repository to show information about the k8s gitlab-runner environment
    as created by https://github.com/rgl/k3s-vagrant

--- create-example-repositories.sh
+++ create-example-repositories.sh
@@ -87,6 +87,43 @@ git push
 popd
 
 # create a new repository with a .gitlab-ci.yml file to show
+# information about the k8s gitlab-runner environment.
+pushd /tmp
+gitlab-create-project gitlab-runner-environment-info-k8s $example_group_id
+git clone https://root:password@$domain/$example_group_name/gitlab-runner-environment-info-k8s.git gitlab-runner-environment-info-k8s && cd gitlab-runner-environment-info-k8s
+# add a file with CR eol terminators to see whether they are preserved.
+printf '1. This line ends with carriage return (CR)\r2. This line ends with carriage return (CR)\r3. This line ends with carriage return (CR)\r' >cr-eol-terminators.md
+# add a file with LF eol terminators to see whether they are preserved.
+printf '1. This line ends with line feed (LF)\n2. This line ends with line feed (LF)\n3. This line ends with line feed (LF)\n' >lf-eol-terminators.md
+# add a file with CRLF eol terminators to see whether they are preserved.
+printf '1. This line ends with carriage return and line feed (CRLF)\r\n2. This line ends with carriage return and line feed (CRLF)\r\n3. This line ends with carriage return and line feed (CRLF)\r\n' >crlf-eol-terminators.md
+# add a file with mixed eol terminators to see whether they are preserved.
+printf '1. This line ends with carriage return (CR)\r2. This line ends with line feed (LF)\n3. This line ends with carriage return and line feed (CRLF)\r\n' >mixed-eol-terminators.md
+# add the .gitlab-ci.yml file.
+cat >.gitlab-ci.yml <<'EOF'
+info:
+  stage: test
+  tags:
+    - k8s
+  script:
+    - cat /proc/self/cgroup | sort
+    - cat /etc/os-release
+    - uname -a
+    - dpkg-query -W -f='${binary:Package}\n' | sort
+    - apt-get update && apt-get install -y file procps
+    - id
+    - pwd
+    - env | sort
+    - mount | sort
+    - ps axuww
+    - file *-eol-terminators.md
+EOF
+git add * .gitlab-ci.yml
+git commit -m 'init'
+git push
+popd
+
+# create a new repository with a .gitlab-ci.yml file to show
 # information about the windows powershell gitlab-runner environment.
 pushd /tmp
 gitlab-create-project gitlab-runner-environment-info-windows-ps $example_group_id
