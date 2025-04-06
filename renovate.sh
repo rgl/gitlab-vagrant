8bdcc85f0e76bf8e2ed51315bf6737a7d1d3532e
Rui Lopes <rgl@ruilopes.com>
2025-04-06 22:11:23 +0000

    upgrade to renovate 39.233.3

--- renovate.sh
+++ renovate.sh
@@ -17,7 +17,7 @@ gitea_version='1.23.6'
 
 # see https://hub.docker.com/r/renovate/renovate/tags
 # renovate: datasource=docker depName=renovate/renovate
-renovate_version='39.215.2'
+renovate_version='39.233.3'
 
 # clean.
 echo 'Deleting existing Gitea...'
