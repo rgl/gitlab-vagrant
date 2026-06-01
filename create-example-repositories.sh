5ad22ceaba7b277160992fef18e8db721bc4bdb5
Rui Lopes <rgl@ruilopes.com>
2026-06-01 06:01:01 +0000

    upgrade to openbao 2.5.4

--- create-example-repositories.sh
+++ create-example-repositories.sh
@@ -152,7 +152,7 @@ test:
           secret/example-gitlab-vault-secrets/main/user
 EOF
 # renovate: datasource=github-releases depName=openbao/openbao
-openbao_version='2.5.3'
+openbao_version='2.5.4'
 sed -i -E \
   -e "s,@@OPENBAO_VERSION@@,$openbao_version,g" \
   -e "s,@@VAULT_ADDR@@,$VAULT_ADDR,g" \
